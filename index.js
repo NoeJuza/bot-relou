eb6cc57c69a00cc9512db447f7baa7e047863dea
NoeJuza <noe.juzan.pro@gmail.com>
2023-03-17 09:48:09 +0100

    added more triggers and reworked feur behavior

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ const reactsQuoi = [
     "https://cdn.discordapp.com/attachments/919656305583603722/1085953209136320572/FMTA-jUXoAcVA1K.png",
     "https://tenor.com/view/feur-quoi-clip-gif-21195505",
     "https://tenor.com/view/feur-gif-24566779",
+    "https://media.discordapp.net/attachments/1026874473816789053/1086191217936699492/ezgif.com-video-to-gif.gif",
     "`feur`",
     "*feur*",
     "**feur**",
     "***feur***",
     "FEUR",
+    "f e u r"
     ]   
-const triggersQuoi = ["quoi","kwa"]
+const triggersQuoi = ["quoi","kwa","koi","koa","qwa","qoi"]
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -23,11 +25,15 @@ client.on('ready', () => {
 client.on('messageCreate', async (message) =>{
     if (message.author.bot) return false;
     let msg = message.content.toLowerCase();
-    if (msg.includes("feur")){
+    if (msg.split(" ").includes("feur")){
         message.delete();
         return false;
     }
-    if (msg.includes("pourquoi")){
+    if (msg.includes("c'est quoi")){
+        await message.reply("c'est feur")
+        return false;
+    }
+    if (msg.includes("pourquoi") || msg.includes("pour quoi")){
         await message.reply("bah pour feur mec")
         return false;
     }
